a677322ff7bbd0ffbd14e5bd7390be62e793082e
Asim Ribo <asim.ribo21@gmail.com>
2026-07-25 20:31:11 +0200

    Remove spring boot test and add simple sanity test

---
+++ Radegast/src/test/java/hr/asimr/radegast/simple/SanityTest.java
@@ -0,0 +1,13 @@
+package hr.asimr.radegast.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SanityTest {
+
+    @Test
+    public void testSanity() {
+        assertEquals(4, 2+2);
+    }
+}

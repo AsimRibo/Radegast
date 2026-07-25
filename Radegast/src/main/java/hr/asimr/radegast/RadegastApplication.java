1e8dea507cb836f73b7b99d13836cec516520d81
Asim Ribo <asim.ribo21@gmail.com>
2026-07-25 17:57:43 +0200

    Add initial commit

---
+++ Radegast/src/main/java/hr/asimr/radegast/RadegastApplication.java
@@ -0,0 +1,13 @@
+package hr.asimr.radegast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadegastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadegastApplication.class, args);
+    }
+
+}

d9d4403441cbf7f524466a9d3da768e1d125d1e7
Asim Ribo <asim.ribo21@gmail.com>
2026-08-03 12:51:50 +0200

    Add courses table

--- database/db_init.sql
+++ database/db_init.sql
@@ -9,9 +9,7 @@ CREATE TABLE IF not EXISTS app_users
     active                    BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                             CONSTRAINT chk_app_users_role CHECK (ROLE IN ('STUDENT',
-                             'TEACHER',
-                             'ADMIN'))
+    CONSTRAINT chk_app_users_role CHECK (ROLE IN ('STUDENT','TEACHER','ADMIN'))
     );
 
 INSERT INTO app_users
@@ -32,3 +30,20 @@ VALUES
      'ADMIN',
      TRUE
     );
+
+CREATE TABLE courses
+(
+    id              bigint generated always AS identity PRIMARY KEY,
+    code            VARCHAR(255) NOT NULL UNIQUE,
+    name            VARCHAR(150) NOT NULL,
+    description     TEXT,
+    teacher_id      BIGINT,
+    capacity        INTEGER,
+    enrollment_open BOOLEAN NOT NULL DEFAULT true,
+    status          VARCHAR(20) NOT NULL DEFAULT 'DRAFT',
+    created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_courses_teacher FOREIGN KEY (teacher_id) REFERENCES app_users(id),
+    CONSTRAINT chk_courses_status CHECK (status IN ('DRAFT', 'ACTIVE','ARCHIVED')),
+    CONSTRAINT chk_courses_capacity CHECK (capacity IS NULL OR capacity > 0)
+);
